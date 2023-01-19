3680dffc59e0db6e2d77ccb3718ab7b3a02c5b0e
Sagar Mittal <mittalsagar006@gmail.com>
2023-01-19 22:43:02 +0530

    used useDispatch & useSelector hook of react-redux

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,14 @@ import { createStore } from 'redux';
 
 const counterReducer = (store = { counter: 0 }, action) => {
   if (action.type === 'increment') {
-    counter = store.counter + 1;
+    return {
+      counter: store.counter + 1,
+    };
+  }
+  if (action.type === 'decrement') {
+    return {
+      counter: store.counter - 1,
+    };
   }
   return store;
 };
